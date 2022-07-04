bd19933ff7605367de2426dbb652882d60e6faca
diana-k22 <75387147+diana-k22@users.noreply.github.com>
2022-07-04 16:06:03 +0400

    added a background and 2 buttons to the profile

---
+++ src/components/ProfilePage/sectionSlider/Button.styled.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+
+export const Button = styled.button`
+    padding: 12px 16px;
+    font-weight: 700;
+    font-size: 14px;
+    line-height: 16px;
+    border: 2px solid #777E91;
+    border-radius: 90px;
+    font-family: 'DM Sans';	
+    background: transparent;
+    color: white;
+
+    .button-text {
+        padding-right: 3px;
+    }
+`
+
+export const EditButton = styled(Button)``
