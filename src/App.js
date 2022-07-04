0191d9312062ed418c1470e250ecef25279f9aa0
Capsonchik <Serega00790@mail.ru>
2022-07-04 12:33:50 +0300

    fix bugs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import Footer from "./components/MainPage/Footer/footer";
+import Footer from "./components/MainPage/Footer/Footer";
 import Header from "./components/MainPage/header/Header";
 
 // ШАБЛОН СТРАНИЦЫ
--- src/components/MainPage/footer/footer.jsx
+++ src/components/MainPage/footer/footer.jsx
@@ -1,4 +1,4 @@
-import s from './footer.module.css'
+import s from './Footer.module.css'
 import { Link } from "react-router-dom";
 import Logo from '../header/LogoComponent/Logo.jsx'
 import circle from '../../Images/footer-button.png'
