3c82a2f2fb4f04bccd1f94a38d76f2f0e5484937
diana-k22 <75387147+diana-k22@users.noreply.github.com>
2022-07-04 17:30:49 +0400

    fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import Footer from "./components/MainPage/Footer/Footer";
+import Footer from "./components/MainPage/footer/Footer";
 import Header from "./components/MainPage/header/Header";
 
 // ШАБЛОН СТРАНИЦЫ
