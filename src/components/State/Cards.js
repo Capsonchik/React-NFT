b80ddbeda6c764471b1f178291f590f523a039cb
Capsonchik <Serega00790@mail.ru>
2022-07-09 12:31:00 +0300

    fix users and add new card obj

---
+++ src/components/State/Cards.js
@@ -0,0 +1,27 @@
+export const Cards = [
+    {
+        img: 'img/cardPhoto/card1.png',
+        cardName: 'Amazing digital art',
+        price: 2.05,
+        value: 'ETH'
+    },
+    {
+        img: 'img/cardPhoto/card2.jpg',
+        cardName: 'Amazing digital art',
+        price: 7.32,
+        value: 'ETH'
+    },
+    {
+        img: 'img/cardPhoto/card3.png',
+        cardName: 'Amazing digital art',
+        price: 4.45,
+        value: 'ETH'
+    },
+    {
+        img: 'img/cardPhoto/card4.png',
+        cardName: 'Amazing digital art',
+        price: 15.015,
+        value: 'ETH'
+    },
+    
+]
