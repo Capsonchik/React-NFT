e5b1b43a7bca05bcf5ba7154f611300afe97d2ba
Capsonchik <Serega00790@mail.ru>
2022-07-09 23:06:03 +0300

    full collection block

--- src/components/HotCollections/hotCollection.styled.js
+++ src/components/HotCollections/hotCollection.styled.js
@@ -4,4 +4,14 @@ export const HotCollectionStyled = styled.section`
     background: #F4F5F6;
     padding-top: 128px;
     padding-bottom: 128px;
+
+    .collections__card {
+        width: 32%;
+    }
+
+    .collections {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
 `
