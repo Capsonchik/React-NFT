997d06455398e25b8768495b0211d89d3f225f62
Capsonchik <Serega00790@mail.ru>
2022-07-11 20:49:55 +0300

    finish main blocks with bugs

--- src/components/Discover/discover.jsx
+++ src/components/Discover/discover.jsx
@@ -2,6 +2,10 @@ import { DiscoverStyled } from "./discover.styled";
 import { Container, Header } from "../GlobalStyle/Styled.Global"
 import { NavLink } from "react-router-dom"
 import { useState } from "react"
+import Card from "../Card/card"
+import {Cards} from "../State/Cards"
+
+
 
 const Discover = () => {
 
@@ -15,6 +19,14 @@ const Discover = () => {
         setActiveLink(false)
     }
 
+    const mapCards = Cards.map(el => {
+        return (
+            <div key={el.id} className="card">
+                <Card data={el}/>
+            </div>
+        )
+    })
+
 
 
     return ( 
@@ -22,7 +34,10 @@ const Discover = () => {
             <Container>
                 <Header>Discover</Header>
                 <div className="discover-nav-block">
-                    <div className="discover-nav-block__added">test</div>
+                    <div className="discover-nav-block__added">
+                        <span>Recently added</span>
+                        <img src="/img/circle-arrow/circle-arrow-down.png" alt="btn-arw-dwn" />
+                    </div>
                     <nav className="discover-nav-block__nav">
                         <NavLink onClick={clickActiveLink} to='/' className={ activeLink ? 'nav-link-active' : 'nav-link'}>All items</NavLink>
                         <NavLink onClick={clickLink} to='/' className={({ isActive }) => (isActive ? 'nav-link-active' : 'nav-link')}>Art</NavLink>
@@ -31,7 +46,33 @@ const Discover = () => {
                         <NavLink onClick={clickLink} to='/' className={({ isActive }) => (isActive ? 'nav-link-active' : 'nav-link')}>Music</NavLink>
                         <NavLink onClick={clickLink} to='/' className={({ isActive }) => (isActive ? 'nav-link-active' : 'nav-link')}>Video</NavLink>
                     </nav>
-                    <button className="discover-nav-block__btn">Btn</button>
+                    <button className="discover-nav-block__btn">
+                        Filter 
+                        <img src="/img/close-icon/close-icon.png" alt="close-btn-con" />
+                    </button>
+                </div>
+                <div className="discover-sort-block">
+                    <div className="discover-sort-block__item">
+                        <span>Prive</span>
+                        <img src="/img/circle-arrow/circle-arrow-down.png" alt="btn-arw-dwn" />
+                    </div>
+                    <div className="discover-sort-block__item">
+                        <span>Likes</span>
+                        <img src="/img/circle-arrow/circle-arrow-down.png" alt="btn-arw-dwn" />
+                    </div>
+                    <div className="discover-sort-block__item">
+                        <span>Creator</span>
+                        <img src="/img/circle-arrow/circle-arrow-down.png" alt="btn-arw-dwn" />
+                    </div>
+                    <div className="discover-sort-block__item">
+                        <span>Price Ranhe</span>
+                        <img src="/img/circle-arrow/circle-arrow-down.png" alt="btn-arw-dwn" />
+                    </div>
+                </div>
+                <div className="discover-cards-block">
+                    <div className="discover-card">
+                        {mapCards}
+                    </div>
                 </div>
             </Container>
         </DiscoverStyled>
--- src/components/Discover/discover.styled.js
+++ src/components/Discover/discover.styled.js
@@ -12,6 +12,28 @@ export const DiscoverStyled = styled.section`
         padding-bottom: 32px;
     }
 
+    .discover-nav-block__added {
+        width: 20%;
+        height: 46px;
+        border: 2px solid #E6E8EC;
+        border-radius: 12px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 8px;
+
+        & img {
+            width: 32px;
+            height: 32px;
+        }
+
+        & span {
+            font-weight: 500;
+            font-size: 14px;
+            color: #23262F;
+        } 
+    }
+
     .nav-link-active {
         font-weight: 700;
         font-size: 14px;
@@ -36,4 +58,55 @@ export const DiscoverStyled = styled.section`
         margin: 0 24px 0 0;
         cursor: pointer;
   }
+
+  .discover-nav-block__btn {
+    width: 13%;
+    height: 48px;
+    border-radius: 90px;
+    background: #3772FF;
+    border: 0;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding-left: 24px;
+    padding-right: 24px;
+    font-weight: 700;
+    font-size: 16px;
+    color: #FCFCFD;
+  }
+
+  .discover-sort-block {
+    margin-top: 32px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .discover-sort-block__item {
+    width: 23%;
+    height: 48px;
+    border: 2px solid #E6E8EC;
+    border-radius: 12px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding-left: 16px;
+    padding-right: 8px;
+
+    & img {
+        width: 32px;
+        height: 32px;
+    }
+  }
+
+  .discover-cards-block {
+    margin-top: 32px;
+  }
+  
+  .discover-card {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+  }
+
 `
