eec46b165c1db91184327f792691f9eb5812a028
Capsonchik <Serega00790@mail.ru>
2022-07-13 18:46:10 +0300

    add drop down in sellers block

--- src/components/PopularSellers/PopularSellers.jsx
+++ src/components/PopularSellers/PopularSellers.jsx
@@ -34,7 +34,7 @@ const [offset, setOffset] = useState(232)
             <div className='section-wrapper'>
                 <Container>
                     <div className='popular-sellers__header'>
-                        <SellersSortBlock/>
+                        <SellersSortBlock />
                     </div>
                     <div className='popular-sellers__slider'>
                         <div onClick={swipeSlideLeft} className='arrow prev-arrow'>
@@ -42,15 +42,13 @@ const [offset, setOffset] = useState(232)
                         </div>
                         <div className='popular-sellers__slider-block'>
                             <div className='slider-wrapper'>
-                            <Sellers/>
+                                <Sellers />
                             </div>
-                            
                         </div>
                         <div onClick={swipeSlideRight} className='arrow next-arrow'>
-                            <img src={PrevArrow} alt="prev-arrov" />
-                            {/* <img src={NextArrow} alt="next-arrov" /> */}
+                            <img src={PrevArrow} alt="prev-arrow" />
                         </div>
-                    </div>                                           
+                    </div>
                 </Container>
             </div>
         </PopularSellersBlock>
