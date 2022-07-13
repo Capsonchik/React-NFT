a5b6b746a27e34d8e81a439cd0ed9b8138e6d72a
Capsonchik <Serega00790@mail.ru>
2022-07-13 14:52:13 +0300

    fix logo styled

--- src/components/MainPage/header/LogoComponent/Logo.jsx
+++ src/components/MainPage/header/LogoComponent/Logo.jsx
@@ -1,13 +1,16 @@
-import s from './Logo.module.css'
+import { LogoStyled } from './logo.styled'
 import logo from '../../../Images/symbol.png'
 import { Link } from 'react-router-dom'
 
+
 const Logo = () => {
     return(
-        <Link to='/' className={s.logo}>
-            <img src={logo} alt='logo'/>
-            <h1>crypter</h1>
-        </Link>
+        <LogoStyled>
+            <Link to='/' className="logo">
+                <img src={logo} alt='logo'/>
+                <h1>crypter</h1>
+            </Link>
+        </LogoStyled>        
     )
 }
 
---
+++ src/components/MainPage/header/LogoComponent/logo.styled.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const LogoStyled = styled.div`
+    .logo {
+        display: flex;
+        text-decoration: none;
+    }
+
+    & img {
+        margin-right: 8px;
+        width: 32px;
+        height: 32px;
+    }
+
+    & h1 {
+        margin-right: 32px;
+        font-size: 24px;
+        color: #23262F;
+    }
+`
